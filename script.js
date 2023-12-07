85e4e9e4cbb1bab9ce172457d4abc2226716bbb3
edbadilla <edbadilla08@gmail.com>
2023-12-07 15:02:00 -0600

    add operate function to js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// let n1 = 1;
+// let n2 = 3;
+// let operator = '+';
+
+
 const add = function(n1, n2) {
 	return n1 + n2;
 };
@@ -14,3 +19,27 @@ const multiply = function(n1, n2) {
 const divide = function(n1, n2) {
     return n1 / n2;
 };
+
+
+const operate = (n1, n2, operator) => {
+    switch (operator) {
+        case "+":
+            return add(n1, n2);
+            break;
+        case "-":
+            return subtract(n1, n2);
+            break;
+        case "*":
+            return multiply(n1, n2);
+            break;
+        case "/":
+            return divide(n1, n2);
+            break;
+        default:
+            return "wrong symbol";
+            break;
+    }
+};
+
+// console.log(add(5,5));
+console.log(operate(5,5,'l'));
